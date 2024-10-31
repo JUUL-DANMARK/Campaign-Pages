0bfacb57007b34b7cf74a96c1a02953ecd0dcaee
Christian Fredegaard <cfr@juuldanmark.dk>
2024-10-31 13:23:22 +0100

    Rebuild Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './*.{html,js}',
-    './**/*.{html,js}',
+    './**/*.html',
   ],
   theme: {
     extend: {
